270e13d8dcce2f6d2987179bcc26a0677c67241b
Artem Petrov <partos0511@gmail.com>
2013-12-21 19:27:26 +0400

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,18 +4,33 @@ set :repo_url, 'git@github.com:artempartos/colibrary.git'
 set :deploy_to, '/u/apps/library'
 set :scm, :git
 
+set :format, :pretty
+set :log_level, :debug
+
 set :use_sudo, false
+
+#raise fetch(:bundle_bins).inspect
+#set :bundle_bins, fetch(:bundle_bins, []) - ['rake']
+
+
+#set :rvm_path, RVM_SYSTEM_PATH
+set :rvm_ruby_version, 'ruby-1.9.3-p327'
+
+set :pty, true
 
 # https://help.github.com/articles/using-ssh-agent-forwarding
 # use "ssh-add ~/.ssh/id_rsa"
 set :ssh_options, :forward_agent => true
 
-set :format, :pretty
 set :keep_releases, 5
 
 #set :shared_files, %w()
 set :linked_files, %w(config/database.yml config/backup.rb)
-set :linked_dirs, %w{bin log public/system}
+set :linked_dirs, %w{public/system log public/assets bin}
+
+set :bundle_binstubs, -> { shared_path.join('bin') }
+
+set :bundle_bins, fetch(:bundle_bins, []) + shared_path.join('bin')
 
 namespace :deploy do
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, 'production'
-set :branch, 'master'
+set :branch, 'feature/authentication'
 
 server '62.76.184.163', user: 'library_production', roles: %w{web app}
