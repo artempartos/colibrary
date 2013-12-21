a5e7178a6dcf855641ca7209ff52361d03de4768
Kirillov Alexander <saratovsource@gmail.com>
2013-12-21 13:24:11 +0400

    add user states

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  state_machine initial: :inactive do
+    state :active
+    state :inactive
+
+    event :activate do
+      transition :inactive => :active
+    end
+
+    event :disactivate do
+      transition :active => :inactive
+    end
+  end
 end
