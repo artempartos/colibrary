f73c6e5ddaa7569a2ca8980d4fad96f54588e7a6
Artem Petrov <partos0511@gmail.com>
2013-12-23 18:21:41 +0400

    add production server as DB

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, 'production'
 set :branch, 'feature/test'
 
-server '62.76.184.163', user: 'library_production', roles: %w{web app}
+server '62.76.184.163', user: 'library_production', roles: %w{web app db}
